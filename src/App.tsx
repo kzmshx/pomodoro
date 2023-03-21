5ada9f8a29be76da474ee09967b4636f804288c4
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-03-21 18:52:11 +0900

    app: display timer, buttons and timer items

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {match} from "ts-pattern";
+
+type TimerItem = {
+    name: string
+    seconds: number
+    active: boolean
+}
+
+const defaultTimerItems: TimerItem[] = [
+    {name: '作業', seconds: 1500, active: false},
+    {name: '短休憩', seconds: 300, active: false},
+    {name: '作業', seconds: 1500, active: false},
+    {name: '短休憩', seconds: 300, active: false},
+    {name: '作業', seconds: 1500, active: false},
+    {name: '短休憩', seconds: 300, active: false},
+    {name: '作業', seconds: 1500, active: false},
+    {name: '長休憩', seconds: 900, active: false},
+]
+
+const TimerButtonMode = {
+    START: 'start',
+    STOP: 'stop',
+} as const
+
+type TimerButtonMode = typeof TimerButtonMode[keyof typeof TimerButtonMode]
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [timerItems, setTimerItems] = useState<TimerItem[]>(defaultTimerItems)
+    const [timerButtonMode, setTimerButtonMode] = useState<TimerButtonMode>(TimerButtonMode.START)
+    const timerButtonLabel = match(timerButtonMode)
+        .with(TimerButtonMode.START, (): string => 'スタート')
+        .with(TimerButtonMode.STOP, (): string => 'ストップ')
+        .exhaustive()
+    const [alarmStopButtonDisabled, setAlarmStopButtonDisabled] = useState(true)
+
+    const handleTimerButtonClick = () => {
+        if (timerButtonMode === TimerButtonMode.START) {
+            setTimerButtonMode(TimerButtonMode.STOP)
+        } else if (timerButtonMode === TimerButtonMode.STOP) {
+            setTimerButtonMode(TimerButtonMode.START)
+        } else {
+            throw new Error('Unknown timer button mode')
+        }
+    }
+
+    const handleAlarmStopButtonClick = () => {
+        setAlarmStopButtonDisabled(true)
+    }
+
+    return (
+        <div>
+            <h1>23:45</h1>
+            <button onClick={handleTimerButtonClick}>{timerButtonLabel}</button>
+            <button onClick={handleAlarmStopButtonClick} disabled={alarmStopButtonDisabled}>アラーム停止</button>
+            <table>
+                <thead>
+                <tr>
+                    <th colSpan={2}>ポモドーロ・メニュー</th>
+                </tr>
+                <tr>
+                    <th>メニュー</th>
+                    <th>時間（min）</th>
+                </tr>
+                </thead>
+                <tbody>
+                {timerItems.map((timer, index) => (
+                    <tr key={index}>
+                        <td>{timer.name}</td>
+                        <td>{timer.seconds / 60}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default App;
