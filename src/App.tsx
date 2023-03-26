1ef0731827e9ababd42c38403119c28c919b3cd5
Kazumasa Hirata <k_hirata@openwork.co.jp>
2023-03-26 18:54:43 +0900

    add: アラームを鳴らす

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,95 @@
 import React, {useEffect, useMemo, useState} from 'react';
 
-function useInterval(callback: Function, delay: number | null) {
-    useEffect(() => {
-        if (delay !== null) {
-            const interval = setInterval(() => {
-                callback()
-            }, delay || 0)
-            return () => clearInterval(interval)
-        }
-    }, [callback, delay])
-}
+const mtos = (minutes: number) => minutes * 60;
+const stoms = (seconds: number) => seconds * 1000;
 
 type TimerItem = {
     name: string
     seconds: number
 }
 
-type CurrentTimer = {
-    index: number
-    remainingSeconds: number
-}
-
 const timerItems: TimerItem[] = [
-    {name: '作業', seconds: 1500},
-    {name: '短休憩', seconds: 300},
-    {name: '作業', seconds: 1500},
-    {name: '短休憩', seconds: 300},
-    {name: '作業', seconds: 1500},
-    {name: '短休憩', seconds: 300},
-    {name: '作業', seconds: 1500},
-    {name: '長休憩', seconds: 900},
+    {name: '作業', seconds: mtos(25)},
+    {name: '短休憩', seconds: mtos(5)},
+    {name: '作業', seconds: mtos(25)},
+    {name: '短休憩', seconds: mtos(5)},
+    {name: '作業', seconds: mtos(25)},
+    {name: '短休憩', seconds: mtos(5)},
+    {name: '作業', seconds: mtos(25)},
+    {name: '長休憩', seconds: mtos(15)},
 ]
 
 function App() {
-    const [timer, setTimer] = useState<CurrentTimer>({
-        index: 0,
-        remainingSeconds: timerItems[0].seconds,
-    })
+    const [timerIndex, setTimerIndex] = useState(0)
+    const [timerRemainingSeconds, setTimerRemainingSeconds] = useState(timerItems[0].seconds)
+    const [timerRunning, setTimerRunning] = useState(false)
+    const [alarmRinging, setAlarmRinging] = useState(false)
+
     const remainingTime = useMemo(
         () => {
-            const minutes = Math.floor(timer.remainingSeconds / 60).toString().padStart(2, '0')
-            const seconds = (timer.remainingSeconds % 60).toString().padStart(2, '0')
+            const minutes = Math.floor(timerRemainingSeconds / 60).toString().padStart(2, '0')
+            const seconds = (timerRemainingSeconds % 60).toString().padStart(2, '0')
             return `${minutes}:${seconds}`
         },
-        [timer]
+        [timerRemainingSeconds]
     )
-
-    const [timerRunning, setTimerRunning] = useState(false)
     const timerButtonLabel = useMemo(
         () => timerRunning ? 'ストップ' : 'スタート',
         [timerRunning]
     )
-
-    const [alarmRinging, setAlarmRinging] = useState(false)
     const alarmStopButtonDisabled = useMemo(
         () => !alarmRinging,
         [alarmRinging]
     )
 
-    useInterval(() => {
-        if (timer.remainingSeconds === 0) {
-            const nextIndex = (timer.index + 1) % timerItems.length
-            setTimer({
-                index: nextIndex,
-                remainingSeconds: timerItems[nextIndex].seconds,
-            })
-            setAlarmRinging(true)
-        } else {
-            setTimer({
-                ...timer,
-                remainingSeconds: timer.remainingSeconds - 1,
-            })
+    const handleTimerButtonClick = () => {
+        setTimerRunning(!timerRunning)
+    }
+
+    const handleAlarmStopButtonClick = () => {
+        setAlarmRinging(false)
+    }
+
+    useEffect(() => {
+        if (timerRunning) {
+            const timeout = setTimeout(() => {
+                if (timerRemainingSeconds <= 0) {
+                    setTimerIndex((timerIndex + 1) % timerItems.length)
+                    setTimerRemainingSeconds(timerItems[timerIndex + 1].seconds)
+                    setAlarmRinging(true)
+                } else {
+                    setTimerRemainingSeconds(timerRemainingSeconds - 1)
+                }
+            }, stoms(1))
+            return () => {
+                clearTimeout(timeout)
+            }
         }
-    }, timerRunning ? 1000 : null)
+    }, [timerIndex, timerRemainingSeconds, timerRunning])
+
+    useEffect(() => {
+        if (alarmRinging) {
+            const audio = new Audio('/sound/coo.mp3');
+            audio.loop = true
+            audio.play()
+
+            const timeout = setTimeout(() => {
+                setAlarmRinging(false)
+            }, stoms(10))
+
+            return () => {
+                audio.pause()
+                clearTimeout(timeout)
+            }
+        }
+    }, [alarmRinging])
 
     return (
         <div>
             <h1>{remainingTime}</h1>
             {alarmRinging && <h1>Ringing</h1>}
-            <button onClick={() => setTimerRunning(!timerRunning)}>{timerButtonLabel}</button>
-            <button onClick={() => setAlarmRinging(false)} disabled={alarmStopButtonDisabled}>アラーム停止</button>
+            <button onClick={handleTimerButtonClick}>{timerButtonLabel}</button>
+            <button onClick={handleAlarmStopButtonClick} disabled={alarmStopButtonDisabled}>アラーム停止</button>
             <table>
                 <thead>
                 <tr>
@@ -88,15 +98,13 @@ function App() {
                 <tr>
                     <th>メニュー</th>
                     <th>時間（min）</th>
-                    <th>起動中</th>
                 </tr>
                 </thead>
                 <tbody>
                 {timerItems.map((t, i) => (
-                    <tr key={i}>
+                    <tr key={i} style={{fontWeight: i === timerIndex ? 'bold' : 'normal'}}>
                         <td>{t.name}</td>
                         <td>{t.seconds / 60}</td>
-                        <td>{i === timer.index ? 'YES' : 'NO'}</td>
                     </tr>
                 ))}
                 </tbody>
